f5d5775455fe4047bb4befa9c6a00859ee68bb4a
Grant Winter <grantrwinter@gmail.com>
2014-03-20 03:40:10 +0000

    finished chapter 9 bonus materials

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -67,8 +67,12 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
 
+    
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
+
+    
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,19 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @counter = increment_counter
   end
+
+  private
+
+  def increment_counter
+      if session[:counter].nil?
+        session[:counter] = 0
+      end
+      session[:counter] += 1
+      if session[:counter] > 5
+        return session[:counter]         
+      end
+    end
+
 end
