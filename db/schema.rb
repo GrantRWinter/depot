561293aef7eddbc86c9c5605b0ffe8a538dce5ac
Grant Winter <grantrwinter@gmail.com>
2014-03-21 22:22:25 +0000

    reformatting of shopping cart view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321024534) do
+ActiveRecord::Schema.define(version: 20140321030332) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
